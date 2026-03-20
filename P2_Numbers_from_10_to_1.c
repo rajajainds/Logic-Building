065c43f8c62728625e54f4d708408b0aad76937d
rajajainds <rajajainds@gmail.com>
2026-03-20 22:06:35 +0530

    Program_2

---
+++ P2_Numbers_from_10_to_1.c
@@ -0,0 +1,14 @@
+//Print all Numbers from 10 to 1 in reverse order using while Loop
+
+#include<stdio.h>
+
+int main (){
+
+    int start = 10;
+    while (start>=1) {
+        printf("%d \n",start);
+        start--;
+    }
+
+    return 0;
+}
