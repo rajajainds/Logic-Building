d582f9669f952c7931f499e54e07bcfc14e500b2
rajajainds <rajajainds@gmail.com>
2026-03-20 22:03:15 +0530

    Program_1

---
+++ P1_Numbers_from_1_to_10.c
@@ -0,0 +1,14 @@
+//Print all Numbers from 1 to 10 using while Loop
+
+#include<stdio.h>
+
+int main (){
+
+    int start = 1;
+    while (start<=10) {
+        printf("%d \n",start);
+        start++;
+    }
+
+    return 0;
+}
