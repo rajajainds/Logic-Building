d9a83bb11a0ffc7de8d733a452fc683f4842f978
rajajainds <rajajainds@gmail.com>
2026-03-21 11:43:22 +0530

    Program_10

---
+++ P10_Product_of_all_digits_of_a_number.c
@@ -0,0 +1,26 @@
+//Calculate and print the product of all digits of a number 
+
+#include<stdio.h>
+
+int main(){
+
+    int numGiven = 1245 ; 
+    int num =numGiven ; //given number , product of all digits in it 
+    int productOfDigits = 0 ; 
+    int digit = 1 ; // this will store each digit one by one 
+    if (num != 0 ){
+    
+    productOfDigits = 1;
+ 
+    while (num){
+        digit = num%10 ;
+        num = num/10 ;
+        productOfDigits *= digit ;
+
+    } 
+    }
+
+    printf("Sum of all digits of number %d is : %d\n",numGiven,productOfDigits);
+
+    return 0;
+}
