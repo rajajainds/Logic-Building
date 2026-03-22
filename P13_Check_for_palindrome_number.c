1bd6bc3d5caf24df60d25fac317abeb1fbb90580
rajajainds <rajajainds@gmail.com>
2026-03-22 13:29:02 +0530

    Program_13

--- P13_Check_for_palindrome_number.c
+++ P13_Check_for_palindrome_number.c
@@ -1,23 +1,23 @@
-//Find and print the reverse of a given number using While Loop
+//Check whether the given number is palindrome 
 
 #include<stdio.h>
 
 int main(){
 
-    int numGiven = 2321 ;
+    int numGiven = 217 ;
     int num = numGiven ; //given number 
     int reverseNum = 0  ;
-
-//logic - here 
-//num = 567
-// num = num/10 => 56 , % gives 7
-// reverseNum = reverseNum*10 + num%10 ;
 
     while (num){
         reverseNum = reverseNum*10 + num%10 ;
         num = num/10 ;
     }
+    
     printf("Reverse of the given number %d is : %d\n",numGiven,reverseNum);
-
+    if (numGiven == reverseNum){
+    printf("Yes it is Palindrome number ");
+    }else {
+        printf("No it is not a Palindrome number ");
+    }
     return 0;
 }
