e81faa815dd77889f21277477a23771947d150bc
rajajainds <rajajainds@gmail.com>
2026-03-24 18:15:00 +0530

    Program_21

---
+++ P21_Square_of_each_number_upto_n.c
@@ -0,0 +1,17 @@
+// Print the square of each number from 1 to n.
+#include<stdio.h>
+
+int main (){
+
+    int start=1;
+    int end =6;
+   
+    while (start<=end){
+
+        printf("%d\n",start*start);
+        start++;
+    }
+
+
+    return 0;
+}
