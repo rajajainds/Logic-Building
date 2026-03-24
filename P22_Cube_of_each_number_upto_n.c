67aff51c8341f5c2b862920f5ef4a2f9eb91992b
rajajainds <rajajainds@gmail.com>
2026-03-24 18:30:00 +0530

    Program_22

---
+++ P22_Cube_of_each_number_upto_n.c
@@ -0,0 +1,18 @@
+// Print the cube of each number from 1 to n.
+
+#include<stdio.h>
+
+int main (){
+
+    int start=1;
+    int end =8;
+   
+    while (start<=end){
+
+        printf("%d\n",start*start*start);
+        start++;
+    }
+
+
+    return 0;
+}
