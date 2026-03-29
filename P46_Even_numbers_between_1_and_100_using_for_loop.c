c550cbd8a29f832a965ebe06046da76006ee03b4
rajajainds <rajajainds@gmail.com>
2026-03-29 18:35:09 +0530

    Program_46

---
+++ P46_Even_numbers_between_1_and_100_using_for_loop.c
@@ -0,0 +1,15 @@
+//print all the even numbers between 1 and 100 using for loop
+
+#include<stdio.h>
+
+int main (){
+
+    for(int i=1;i<100;i++){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
+
+
+    return 0;
+}
