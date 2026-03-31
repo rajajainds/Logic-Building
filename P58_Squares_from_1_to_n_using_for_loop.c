27c8aa4571cdb143b5bf13057070e5e0290387c6
rajajainds <rajajainds@gmail.com>
2026-03-31 17:04:52 +0530

    Program_58

---
+++ P58_Squares_from_1_to_n_using_for_loop.c
@@ -0,0 +1,15 @@
+//print the squares of numbers from 1 upto n using for loop
+
+#include<stdio.h>
+
+int main(){
+
+    int start=1;
+    int end =12;
+
+    for(int i =start;i<=end;i++){
+        printf("%d\n",(i*i));
+    }
+
+    return 0;
+}
