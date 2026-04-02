e9bf9055f266c81e64637ce3203c95df8f9110dd
rajajainds <rajajainds@gmail.com>
2026-04-02 22:40:36 +0530

    Program_69

---
+++ P69_Print_1_to_100_break_17.c
@@ -0,0 +1,21 @@
+//print from 1 to 100 and stop if number is divisible by 17
+
+#include<stdio.h>
+
+int main(){
+
+    int numStop =17; // number at which we have to break the loop
+
+    for(int i=1;i<=100;i++){
+        printf("%d\n",i);
+
+        if(i%17==0){
+            break;
+        }
+        }
+
+
+
+
+    return  0;
+}
