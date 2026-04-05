977ae548477fc86d198425f5cd3f895ec6d0326f
rajajainds <rajajainds@gmail.com>
2026-04-05 13:02:18 +0530

    Program_82

--- P82_Series_of_cubes_first_n_terms_print.c
+++ P82_Series_of_cubes_first_n_terms_print.c
@@ -1,16 +1,16 @@
-// find value of series of squares of natural numbers 
+// find value of series of subes of natural numbers 
 
 #include<stdio.h>
 #include<math.h>
 
 int main(){
 
-    int terms =7;
+    int terms =5;
 
     int valusOfSeries=0;
 
     for(int i=1;i<=terms;i++){
-        valusOfSeries+=pow(i,2);
+        valusOfSeries+=pow(i,3);
     }
 
     printf("The valus of this series is %d",valusOfSeries);
