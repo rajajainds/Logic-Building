4e9e91a42abb245b1ca9e83e4f87e9b06fac9e20
rajajainds <rajajainds@gmail.com>
2026-04-07 19:27:02 +0530

    Program_94

---
+++ P94_Print_ith_row_with_iXi.c
@@ -0,0 +1,15 @@
+// print the pattern where ith row has value i x i
+
+#include<stdio.h>
+
+int main(){
+
+    int num=4;
+
+    for(int i=1;i<=num;i++){
+        printf("%d row --> %d\n",i,i*i);
+
+    }
+
+    return 0;
+}
