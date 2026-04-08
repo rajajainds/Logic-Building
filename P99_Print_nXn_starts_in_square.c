98ad38c3956f8465265272366bf9bb0d5f106ec3
rajajainds <rajajainds@gmail.com>
2026-04-08 17:43:22 +0530

    Program_99

---
+++ P99_Print_nXn_starts_in_square.c
@@ -0,0 +1,21 @@
+//print n X n stars in a square form
+
+#include<stdio.h>
+
+
+int main(){
+
+    int n=8;
+
+
+for(int i=0;i<n;i++){
+    printf("\n");
+    for(int i=0;i<n;i++){
+
+        printf("*");
+
+    }
+}
+
+    return 0;
+}
