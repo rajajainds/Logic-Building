c39b0950437af8b92da18ff3ed2d2c8939d0ecfa
rajajainds <rajajainds@gmail.com>
2026-04-08 17:45:47 +0530

    Program_100

---
+++ P100_Print_trinagle_of_stars.c
@@ -0,0 +1,21 @@
+//print triangle of stars from 1 to n
+
+#include<stdio.h>
+
+int main(){
+
+      int n=8;
+
+
+for(int i=1;i<=n;i++){
+    printf("\n");
+    for(int j=1;j<=i;j++){
+
+        printf("*");
+
+    }
+}
+
+
+    return 0;
+}
