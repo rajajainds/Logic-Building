a2c3bc8275d0842e8997d92398267b1877da92a0
rajajainds <rajajainds@gmail.com>
2026-04-09 22:36:02 +0530

    Program_102

---
+++ P102_Print_stars_in_even_numbers.c
@@ -0,0 +1,21 @@
+//print stars in even numbers 
+
+#include<stdio.h>
+
+int main(){
+
+         int n=8;
+
+for(int i=1;i<=n;i++){
+    printf("\n");
+    for(int j=1;j<=i;j++){
+
+        printf("**");
+
+    }
+}
+
+
+
+    return 0;
+}
