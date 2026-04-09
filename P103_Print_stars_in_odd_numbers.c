b1cc4f9490fd4893a878cdf74a943de0ce9c70d3
rajajainds <rajajainds@gmail.com>
2026-04-09 22:39:40 +0530

    Program_103

---
+++ P103_Print_stars_in_odd_numbers.c
@@ -0,0 +1,24 @@
+//print stars in odd numbers 
+
+#include<stdio.h>
+
+int main(){
+
+       int n=8;
+
+for(int i=1;i<=n;i++){
+    printf("\n");
+    for(int j=1;j<=(2*i-1);j++){
+
+        printf("*");
+
+    }
+}
+
+
+
+
+
+    return 0;
+
+}
